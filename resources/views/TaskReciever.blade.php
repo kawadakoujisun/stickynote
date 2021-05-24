9a8c73381d1207364cc4e9c458584ccc07bab438
kouji.kawada <kawadakoujisun@gmail.com>
2021-05-24 13:43:13 +0000

    modify js assign

--- resources/views/TaskReciever.blade.php
+++ resources/views/TaskReciever.blade.php
@@ -27,6 +27,6 @@
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
         <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
         
-        <script src="/js/app.js"></script>
+        <script src="{{ asset('/js/app.js') }}"></script>
     </body>
 </html>
